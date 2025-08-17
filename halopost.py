1af1c07631227982557d1ced3f9de0f0d783f523
Mendy Green <greenlighttec@users.noreply.github.com>
2025-08-17 14:11:58 -0400

    Add additional Halo API methods

--- halopost.py
+++ halopost.py
@@ -5,11 +5,31 @@ def setclients(client):
     resource = '/api/client'
     postBody = client
     for listItem in postBody:
-        if not listItem['id']:
+        if not listItem.get('id'):
             print('Client ID must be specified when updating a client')
             return False
     else:
         return halopost(resource,postBody)
+
+def setusers(user):
+    resource = '/api/users'
+    postBody = user
+    for listItem in postBody:
+        if not listItem.get('id'):
+            print('User ID must be specified when updating a user')
+            return False
+    else:
+        return halopost(resource, postBody)
+
+def settickets(ticket):
+    resource = '/api/tickets'
+    postBody = ticket
+    for listItem in postBody:
+        if not listItem.get('id'):
+            print('Ticket ID must be specified when updating a ticket')
+            return False
+    else:
+        return halopost(resource, postBody)
 
 
 
